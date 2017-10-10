4a7e9480db97b9ef2441d6a6558f6a1e188e934d
Soft Mali <gabrielbecher@alunos.utfpr.edu.br>
2017-10-10 16:39:26 -0300

    Produtos

---
+++ produto.php
@@ -0,0 +1,35 @@
+<section>
+    <figure>  
+      <strong>Sapatilha Oncinha</strong>
+      <img src="https://http2.mlstatic.com/D_Q_NP_752553-MLB25527513803_042017-Q.jpg" alt="janis">
+      <em>R$50,00</em>
+      <p>Aenean tellus sapien, cursus ut ullamcorper eu, congue vestibulum libero. Praesent at semper ipsum. Pellentesque tempus, libero non varius sollicitudin, mi tortor tristique metus, viverra ullamcorper lorem ligula quis tortor.</p>
+  </figure>
+    <span>Cores:</span>
+
+      <select>
+      <option value="volvo">azul</option>
+      <option value="saab">vermelho</option>
+      <option value="mercedes">amarelo</option>
+      <option value="audi">cinza</option>
+      </select>
+  
+   <details>
+      <summary>Detalhes sobe o produto</summary>
+      <p> * não lave com agua</p>
+      <p> * Garantia de 1ano </p>
+  </details> 
+  <form oninput="x.value=parseInt(a.value)+parseInt(b.value)">
+    <label>Quantidade:
+    0<input type="range" id="a" value="50">100
+    </label>
+    <label>Preço:
+    <input type="number" min="0.00" max="10000.00" step="0.01" readonly="true" value="50.55"/>
+    </label>
+    <label> =
+    <output name="x" for="a b"></output>
+    </label>
+    <button type="submit">Adicionar no Carrinho</button>
+  </form>
+  
+</section>
